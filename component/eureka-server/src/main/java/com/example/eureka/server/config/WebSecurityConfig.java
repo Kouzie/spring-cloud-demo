5000051feff45bc26f98c1ba203286f032bab3e1
Giyong Ko <kgy1996@naver.com>
2023-04-28 20:10:47 +0900

    update

---
+++ component/eureka-server/src/main/java/com/example/eureka/server/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.eureka.server.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 다른 service 에서 접근 허용을 위해 csrf 제거
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
